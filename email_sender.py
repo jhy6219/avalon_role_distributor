944575094242e32722cc3e467f4ab0304f910bd7
seo <cv5006@naver.com>
2025-06-05 00:05:10 +0900

    add email sender

---
+++ email_sender.py
@@ -0,0 +1,131 @@
+import csv
+import smtplib
+import ssl
+import uuid
+
+import mimetypes
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+
+from dominate import document
+from dominate.tags import *
+
+# Gmail SMTP server
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+
+
+class EmailSender:
+    def __init__(self, sender_addr:str, receivers_file:str|None=None):
+        self.msg = MIMEMultipart('related')
+        self.msg["From"] = sender_addr
+        
+        self.doc = document(title='html_doc')
+        
+        self.receivers = dict()
+        if receivers_file is not None:
+            self.load_email_list(receivers_file)
+
+
+    def add_receiver(self, nickname:str, email_addr:str):
+        self.receivers[nickname] = email_addr
+
+
+    def load_email_list(self, csv_path:str):
+        try:
+            with open(csv_path, mode='r', newline='', encoding='utf-8') as csvfile:
+                csv_reader = csv.reader(csvfile)
+
+                for row in csv_reader:
+                    nick = row[0].strip()
+                    addr = row[1].strip()
+                    self.add_receiver(nick, addr)
+
+        except FileNotFoundError:
+            print(f"Error: File not found '{csv_path}'")
+        except IndexError:
+            print(f"Error: Column index out of range for file '{csv_path}'. Check key_column_index and value_column_index.")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+    
+
+    def add_img(self, img_path:str):
+        img_cid = str(uuid.uuid4())
+
+        # Edit Doc
+        with self.doc:
+            div(img(src=f'cid:{img_cid}'))
+
+        # Attach Img
+        try:
+            with open(img_path, 'rb') as fp:
+                img_data = fp.read()
+
+            _, subtype = mimetypes.guess_type(img_path)[0].split('/')
+            msg_image = MIMEImage(img_data, _subtype=subtype)
+            msg_image.add_header('Content-ID', f'<{img_cid}>')
+            self.msg.attach(msg_image)
+
+        except FileNotFoundError:
+            print(f"Error: Image file '{img_path}' not found.")
+        except TypeError: # mimetypes.guess_type might return None if it can't guess
+            print(f"Error: Could not determine MIME type for image '{img_path}'. Is it a valid image file?")
+        except Exception as e:
+            print(f"Error preparing image attachment: {e}")
+
+
+    def add_heading(self, txt:str, level:int=4):
+        with self.doc:
+            if level == 1:
+                h1(txt)
+            elif level == 2:
+                h2(txt)
+            elif level == 3:
+                h3(txt)
+            elif level == 4:
+                h4(txt)
+
+
+    def add_text(self, txt:str):
+        with self.doc:
+            p(txt)
+
+
+    def send(self, pw:str, subject:str, receiver_addr:str):
+        try:
+            with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as srv:
+                sender_addr = self.msg["From"]
+                self.msg["To"] = receiver_addr
+                self.msg["Subject"] = subject
+                self.msg.attach(MIMEText(self.doc.render(), 'html'))
+
+                context = ssl.create_default_context()
+                srv.starttls(context=context)
+                srv.login(sender_addr, password=pw)
+                srv.send_message(self.msg)
+                print("Email sent successfully!")
+                
+        except Exception as e:
+            print(f"Error sending email: {e}")
+
+
+if __name__ == "__main__":
+    addr = input("insert email: ")
+    pw = input("insert password: ")
+
+    es = EmailSender(
+        sender_addr=addr,
+        receivers_file='./config/receivers.csv'
+    )
+    es.add_receiver('player3', 'cv5006@naver.com')
+
+    es.add_img('./media/test_img.png')
+    for i in range(1, 5):
+        es.add_heading(f'h{i} text', i)
+        es.add_text(f'text {i}')
+
+    import time
+    for name, addr in es.receivers.items():
+        es.send(pw, f"test for {name}", addr)
+        time.sleep(3)
