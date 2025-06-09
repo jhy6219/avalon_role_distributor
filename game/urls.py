7763f8cba5819ab2106c808ffbe961b139ae0c38
hwiwon <hwiwon@hanwha.com>
2025-06-09 17:58:14 +0900

    simplifying

--- game/urls.py
+++ game/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('create_session/', views.create_session, name='create_session'),
-    path('join_session/<uuid:session_id>/', views.join_session, name='join_session'),
-    path('game_lobby/<uuid:session_id>/', views.game_lobby, name='game_lobby'),
+    path('join/<uuid:session_id>/', views.join, name='join'),
+    path('lobby/<uuid:session_id>/', views.lobby, name='lobby'),
 ]
