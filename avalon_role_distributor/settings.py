89b948db3bcc52eb8685d6388fca3fa81a0d0e81
merlin <jhy6219@naver.com>
2025-06-13 23:57:15 +0900

    allow server IP only

--- avalon_role_distributor/settings.py
+++ avalon_role_distributor/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = 'django-insecure-4!sf%5!a&1_39d89($-&-#77&(5s_3318(=2x=#pbvykj(dp(q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '211.188.61.40'
+]
 
 
 # Application definition
